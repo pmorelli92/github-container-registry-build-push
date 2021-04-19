ca0b3d73203e6b470d5e14f8dd19e90751fa0d46
Miniland1333 <Miniland1333@users.noreply.github.com>
2021-04-19 09:39:13 -0700

    Swapped GITHUB_ACTOR to GITHUB_OWNER

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,10 @@ BUILD_CONTEXT=$5
 # Login to GHCR
 echo ${PERSONAL_ACCESS_TOKEN} | docker login https://ghcr.io -u ${GITHUB_ACTOR} --password-stdin
 
+GITHUB_OWNER=`echo ${GITHUB_REPOSITORY} | cut -d/ -f1`
+
 # Set up full image with tag
-IMAGE_ID=ghcr.io/${GITHUB_ACTOR}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
 
 #TODO REMOVE
