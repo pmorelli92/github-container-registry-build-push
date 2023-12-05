58bce488bea7abe48cc7d07f457947d48217fec3
Pablo Morelli <pabmorelli@gmail.com>
2023-12-05 15:18:27 +0100

    removed debug echos

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,14 +19,12 @@ IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
 
 # Format the docker build arguments
-echo $DOCKER_BUILD_ARGS
 IFS=$',' read -a items <<< "$DOCKER_BUILD_ARGS"
 BUILD_ARGS=''
 for element in "${items[@]}"
 do
 	if [ "$element" ];then
 		BUILD_ARGS="$BUILD_ARGS --build-arg \"$element\""
-		echo $BUILD_ARGS
 	fi
 done
 
